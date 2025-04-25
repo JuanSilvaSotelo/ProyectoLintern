0c8982a5208cdc5de910956cab8fe4669dac633f
JuanSilva <juan_dsilvas@soy.sena.edu.co>
2025-04-25 01:20:39 -0500

    Webpack y deploy actualizado

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: 'development',
   entry: {
     index: './src/main.js',
     style: './src/assets/styles.css',
@@ -13,7 +13,6 @@ module.exports = {
     static: './dist',
     hot: true,
     liveReload: true,
-    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
